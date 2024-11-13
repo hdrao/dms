5bc0f713064edb82b7902eb1b5166adc8caae669
Rao Danish <danishrao299@gmail.com>
2024-11-13 15:43:19 +0500

    Flow Completed

---
+++ src/app/signin/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+
+export default function  signin() {
+    return (
+        <div className=" flex container min-h-screen justify-center items-center mx-auto">
+            <Button variant="outline">Continue With Google</Button>
+        </div>
+    )
+} 
