9a0f784baa70f97a859f2cd31b2567547ece7f67
Rao Danish <danishrao299@gmail.com>
2024-11-15 00:39:34 +0500

    Normalize line endings to LF using .gitattributes

--- src/components/Header/page.js
+++ src/components/Header/page.js
@@ -10,11 +10,14 @@ import {
   import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import Link from "next/link"
 import { Button } from "../ui/button";
+import { auth } from "../../../auth";
 
 
 
 export default function Header () {
-    const session = null;
+    const session = auth();
+    console.log("session of login",auth());
+    
 
     return (
     <div className=" bg-secondary py-2 px-5 ">
